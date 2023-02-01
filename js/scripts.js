04980fa6b53350affa8e5885a8b2bc5e089ab359
fjam2023 <fatima.jamankarayeva@gmail.com>
2023-01-31 19:20:12 -0800

    added else to if statement
    else was added to have line breaks

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,12 @@ let pokemonList=[
   ];
 
   for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name+" "+"(height:"+pokemonList[i].height+")"+"<br>");
+    document.write(pokemonList[i].name+" "+"(height:"+pokemonList[i].height+")");
     
     if(pokemonList[i].height>1.0){
       document.write("-Wow, that's big! "+"<br>");
+    } else{
+      document.write(" "+"<br>");
     }
 
   }
