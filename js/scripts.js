d171e5d0892c97a6fbdfc92ff816dbe51301e60c
fjam2023 <fatima.jamankarayeva@gmail.com>
2023-01-31 18:41:32 -0800

    Added changes to display text
    Added for loop to reiterate pokemon names, if statement to display which pokemon has highest height. Additionally, adjusted indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 let pokemonList=[
-    {name:"vileplume", height:1.2, type:['grass', 'poison']},
-    {name:"metapod", height:0.7, type:"bug"},
-    {name:"wartortle", height:1, type:"water"}
+    {
+      name:"vileplume",
+      height:1.2, 
+      type:['grass', 'poison']
+    },
+    {
+      name:"metapod", 
+      height:0.7, 
+      type:"bug"
+    },
+    {
+      name:"wartortle", 
+      height:1, 
+      type:"water"
+    }
   ];
+
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name+" "+"(height:"+pokemonList[i].height+")"+"<br>");
+    
+    if(pokemonList[i].height>1.0){
+      document.write("-Wow, that's big! "+"<br>");
+    }
+
+  }
+
+
+  
