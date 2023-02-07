218e2c56b32c529692fe4d6e0d3ffc3f048b3826
fjam2023 <fatima.jamankarayeva@gmail.com>
2023-02-07 10:47:58 -0800

    added event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,14 @@
 let pokemonRepository = (function(){
   let pokemonList=[
-    {
-      name:"vileplume",
+    {name:"vileplume",
       height:1.2, 
-      type:['grass', 'poison']
-    },
-    {
-      name:"metapod", 
+      type:['grass', 'poison']},
+    {name:"metapod", 
       height:0.7, 
-      type:"bug"
-    },
-    {
-      name:"wartortle", 
+      type:"bug"},
+    {name:"wartortle", 
       height:1, 
-      type:"water"
-    }
+      type:"water"}
   ]
 
  function getAll () {
@@ -27,23 +21,33 @@ let pokemonRepository = (function(){
 function addListItem(pokemon){
   let pokemonList = document.querySelector(".pokemon-list");
   let listpokemon = document.createElement("li");
+  
   let button = document.createElement("button");
+  button.addEventListener('click', function(showDetails){
+    console.log(showDetails);
+  });
+  
   button.innerText = pokemon.name;
   button.classList.add("button-class"); 
   listpokemon.appendChild(button);
   pokemonList.appendChild(listpokemon);
 
+
+}
+
+function showDetails(pokemon){
+  console.log(pokemon);
 }
 
   return {
     getAll: getAll,
     add: add,
-    addListItem: addListItem
+   addListItem: addListItem,
+   showDetails: showDetails
     }
 })()
 
 pokemonRepository.getAll().forEach(function(pokemon){
  pokemonRepository.addListItem(pokemon);
-
- 
+  pokemonRepository.showDetails(pokemon);
 });
